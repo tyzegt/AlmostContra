c19d4ec303b192f56146b1d80b78bf9ad6721218
Tyzegt <admin@almostgames.ru>
2018-04-29 22:20:29 +0700

    AlmostContra #25

---
+++ Assets/Scripts/AngleChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleChecker : MonoBehaviour {
+
+    private PlayerController player;
+    private float angle;
+    private Vector2 A, B, C;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    public float checkAngle()
+    {
+        A = new Vector2(transform.position.x, transform.position.y);
+        B = new Vector2(player.transform.position.x, player.transform.position.y);
+        C = B - A;
+
+        angle = Mathf.Atan2(C.y, C.x) * Mathf.Rad2Deg;
+        angle = Mathf.Round(angle / 30) * 30;
+
+        return angle;
+    }
+
+}
---
+++ Assets/Scripts/EnemyWallCannon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWallCannon : MonoBehaviour {
+
+    private AngleChecker angler;
+
+    void Start()
+    {
+        angler = GetComponent<AngleChecker>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angler.checkAngle());
+	}
+}
