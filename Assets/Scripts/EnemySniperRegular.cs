628cdad08d6720f0ba018cc6db5af06a22cb396f
Tyzegt <admin@almostgames.ru>
2018-08-05 21:50:13 +0700

    AlmostContra #28

---
+++ Assets/Scripts/EnemySniperRegular.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySniperRegular : MonoBehaviour {
+
+    private AngleChecker angler;
+    private Animator anim;
+
+
+    // Use this for initialization
+    void Start () {
+        angler = GetComponent<AngleChecker>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(FindObjectOfType<PlayerController>().transform.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        } else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        anim.SetInteger("Angle",(int) angler.checkAngle());
+	}
+}
